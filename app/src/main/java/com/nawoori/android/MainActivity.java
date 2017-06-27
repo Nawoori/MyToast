bbc1bb212b740e08b6e5de9adce17fd2c04f2d0e
good edu <goodeducoop@gmail.com>
2017-06-28 01:40:13 +0900

    Toast message 위치바꾸기

---
+++ app/src/main/java/com/nawoori/android/MainActivity.java
@@ -0,0 +1,30 @@
+package com.nawoori.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "위치가 바뀐 토스트", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP|Gravity.LEFT, 200, 200);
+                toast.show();
+
+            }
+        });
+    }
+}
