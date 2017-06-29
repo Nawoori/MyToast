0c9a6706a69dfd00219234f6756a7f35dad0b915
good edu <goodeducoop@gmail.com>
2017-06-29 22:06:50 +0900

    Toast

--- app/src/main/java/com/nawoori/android/MainActivity.java
+++ app/src/main/java/com/nawoori/android/MainActivity.java
@@ -1,15 +1,19 @@
 package com.nawoori.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+    Button button, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(), "위치가 바뀐 토스트", Toast.LENGTH_LONG);
-                toast.setGravity(Gravity.TOP|Gravity.LEFT, 200, 200);
-                toast.show();
+                LayoutInflater inflater = getLayoutInflater();
+                View layout = inflater.inflate(R.layout.toastborder, (ViewGroup) findViewById(R.id.toast_layout_root));
+                TextView text = (TextView) layout.findViewById(R.id.text);
+                text.setText("모양을 바꾼 토스트");
+                Toast toast = new Toast(getApplicationContext());
+                toast.setGravity(Gravity.CENTER, 0, -100);
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setView(layout);
 
+                toast.show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "스넥바입니다.", Snackbar.LENGTH_LONG).show();
             }
         });
     }
